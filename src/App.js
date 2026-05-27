3a3601eb7b47165e68e45d490d686de974a3b926
Niya16-ai <rukwong.uru@gmail.com>
2026-05-28 02:20:29 +0700

    Update frontend with new code

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
 
     
     try {
-      const res = await axios.post('http://localhost:8000/predict', formData);
+      const res = await axios.post('http://localhost:8000/', formData);
       setResults(res.data.results);
     } catch (err) {
       console.error("API error:", err);
